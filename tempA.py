fb44a9135063385e223185fbff714fb00eeba66b
koudo88 <kdorinelaua@gmail.com>
2025-12-06 07:13:40 +0100

    just want to commit local

--- tempA.py
+++ tempA.py
@@ -1,25 +1,29 @@
 temperature = [14,18,21,20,19,16,15]
 
 #durchschnitt berechnen
-def durchschnitt(werte):
-return sum(werte)/len(werte)
+def durchschnitt(temperature):
+  print(sum(temperature)/len(temperature))
 
 #hoechste und niedrige temperature 
-def tempspannweite(werte)
-return max(werte), min(werte)
+def maxtempspannweite(temperature):
+  print(max(temperature))
+
+def mintempspannweite(temperature):
+    print( min(temperature))
 
 #pruefen,ob die Durchschnittstemperatur warm oder kalt ist
-def gradtemp(grad):
-if grad > 20:
-return "Woche war warm!"
-else:
-return "Woche war kuehl"
+def grad_temp(temperature):
+  if temperature > 20:
+    print("Woche war warm!")
+
+print("Woche war kuehl")
 
 if __name__ == "__main__":
   grad_temp = durchschnitt(temperature)
-  max_temp, min_temp = tempspannweite(temperature)
+  max_temp = maxtempspannweite(temperature)
+  min_temp = mintempspannweite(temperature)
 
-print(f"Durchschnittstemparatur: {grad_temp:.2f} °c")
+print(f"Durchschnittstemparatur: {grad_temp} °c")
 print(f"Höchste Temperatur : {max_temp}")
 print(f"Niedrigste Temperatur: {min_temp}")
 print(f"gradtemp(grad_temp)")
